7e15ced5f1924b5096081d9701da63ba6d3b225d
CYNO-MC <73330031+CYNO-MC@users.noreply.github.com>
2020-10-25 18:03:44 -0400

    Correction to Value
    .25 - 0.25 (probably doesn't matter, entity is still very fast)

--- src/main/java/com/cyno/diablo/entities/DiabloEntity.java
+++ src/main/java/com/cyno/diablo/entities/DiabloEntity.java
@@ -52,7 +52,7 @@ public class DiabloEntity extends MonsterEntity implements IAnimatedEntity {
     public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
         return DiabloEntity.registerAttributes()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 100.0)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, .25f)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.25f)
                 .createMutableAttribute(Attributes.ATTACK_KNOCKBACK, 10.0)
                 .createMutableAttribute(Attributes.ATTACK_DAMAGE, 15.0)
                 .createMutableAttribute(Attributes.FOLLOW_RANGE, 50.0);
