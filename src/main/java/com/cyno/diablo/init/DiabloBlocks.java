1c333a8aa61415e4ebf058012b64f082ea22c8e7
CYNO-MC <73330031+CYNO-MC@users.noreply.github.com>
2020-10-25 21:02:48 -0400

    Complete Revision of How Blocks are Registered
    takes place in the Init Package, Diablo

--- src/main/java/com/cyno/diablo/init/DiabloBlocks.java
+++ src/main/java/com/cyno/diablo/init/DiabloBlocks.java
@@ -1,11 +1,12 @@
 package com.cyno.diablo.init;
 
 import com.cyno.diablo.Diablo;
-import com.cyno.diablo.blocks.AncientPillar;
-import com.cyno.diablo.blocks.NetherBrickPlankBlock;
-import com.cyno.diablo.blocks.ReinforcedNetherBrickBlock;
-import com.cyno.diablo.blocks.WarpedGlowstoneBlock;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.RotatedPillarBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,8 +19,28 @@ public class DiabloBlocks {
 
 
     //Blocks
-    public static final RegistryObject<Block> REINFORCED_NETHER_BRICK_BLOCK = BLOCKS.register("reinforced_nether_brick_block", ReinforcedNetherBrickBlock:: new);
-    public static final RegistryObject<Block> NETHER_BRICK_PLANK_BLOCK = BLOCKS.register("nether_brick_plank_block", NetherBrickPlankBlock:: new);
-    public static final RegistryObject<Block> WARPED_GLOWSTONE_BLOCK = BLOCKS.register("warped_glowstone_block", WarpedGlowstoneBlock:: new);
-    public static final RegistryObject<Block> ANCIENT_PILLAR = BLOCKS.register("ancient_pillar", AncientPillar:: new);
-}
+    public static final RegistryObject<Block> REINFORCED_NETHER_BRICK_BLOCK = BLOCKS.register("reinforced_nether_brick_block", () -> new Block(Block.Properties.create(Material.IRON)
+            .hardnessAndResistance(50.0f, 1200f)
+            .sound(SoundType.METAL)
+            .harvestLevel(3)
+            .harvestTool(ToolType.PICKAXE)));
+
+    public static final RegistryObject<Block> NETHER_BRICK_PLANK_BLOCK = BLOCKS.register("nether_brick_plank_block", () -> new Block(Block.Properties.create(Material.IRON)
+            .hardnessAndResistance(2.0f, 3.0f)
+            .sound(SoundType.STONE)
+            .harvestLevel(2)
+            .harvestTool(ToolType.PICKAXE)));
+
+    public static final RegistryObject<Block> WARPED_GLOWSTONE_BLOCK = BLOCKS.register("warped_glowstone_block", () -> new Block(Block.Properties.create(Material.GLASS)
+            .sound(SoundType.GLASS)
+            .harvestLevel(0)
+            .harvestTool(ToolType.PICKAXE)
+            .hardnessAndResistance(0.3f,0.3f)
+            .setLightLevel(value -> 15)));
+
+    public static final RegistryObject<Block> ANCIENT_PILLAR = BLOCKS.register("ancient_pillar", () -> new RotatedPillarBlock(AbstractBlock.Properties.create(Material.IRON)
+            .hardnessAndResistance(0.8f, 0.2F)
+            .sound(SoundType.ANCIENT_DEBRIS)
+            .harvestLevel(3)
+            .harvestTool(ToolType.PICKAXE)
+            .setRequiresTool()));}
--- src/main/java/com/cyno/diablo/init/DiabloItems.java
+++ src/main/java/com/cyno/diablo/init/DiabloItems.java
@@ -4,36 +4,45 @@ import com.cyno.diablo.Diablo;
 import com.cyno.diablo.blocks.BlockItemBase;
 import com.cyno.diablo.items.ItemBase;
 import com.cyno.diablo.util.enums.DiabloItemTier;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DiabloItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Diablo.MOD_ID);
 
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Diablo.MOD_ID);
 
     // Items
     public static final RegistryObject<Item> GLASS_VILE = ITEMS.register("glass_vile", ItemBase::new);
     public static final RegistryObject<Item> THE_HORNS_OF_DIABLO = ITEMS.register("the_horns_of_diablo", ItemBase::new);
 
-
     //Tools
     public static final RegistryObject<SwordItem> THE_CONFLAGRATE = ITEMS.register("the_conflagrate",
-            () ->
-            new SwordItem(DiabloItemTier.THE_CONFLAGRATE, 9, +3.0f, new Item.Properties().group(Diablo.TAB)));
+            () -> new SwordItem(DiabloItemTier.THE_CONFLAGRATE, 9, +3.0f, new Item.Properties().group(Diablo.TAB)));
 
-    //Block Items
+    @SubscribeEvent
+    public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
+        final IForgeRegistry<Item> registry = event.getRegistry();
 
-    public static final RegistryObject<Item>REINFORCED_NETHER_BRICK_BLOCK_ITEM = ITEMS.register("reinforced_nether_brick_block",
-            () -> new BlockItemBase(DiabloBlocks.REINFORCED_NETHER_BRICK_BLOCK.get()));
-    public static final RegistryObject<Item>NETHER_WALL_BLOCK_ITEM = ITEMS.register("nether_brick_plank_block",
-            () -> new BlockItemBase(DiabloBlocks.NETHER_BRICK_PLANK_BLOCK.get()));
-    public static final RegistryObject<Item>WARPED_GLOWSTONE_BLOCK_ITEM = ITEMS.register("warped_glowstone_block",
-            () -> new BlockItemBase(DiabloBlocks.WARPED_GLOWSTONE_BLOCK.get()));
-    public static final RegistryObject<Item>ANCIENT_PILLAR_ITEM = ITEMS.register("ancient_pillar",
-            () -> new BlockItemBase(DiabloBlocks.ANCIENT_PILLAR.get()));
+        // for each block we registered...
+        DiabloBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach( (block) -> {
+            // make a block item that places the block
+            final BlockItem blockItem = new BlockItemBase(block);
+
+            // register the block item with the same name as the block
+            blockItem.setRegistryName(block.getRegistryName());
+            registry.register(blockItem);
+        });
+    }
+
 
 
 
