7ab27174eea53b26d9f605f890de661383cfa297
Luke Graham Landry <graham11000@gmail.com>
2020-10-28 16:28:39 -0400

    Degrees & Collisions
    Fire Particle
    - switched direction to degrees
    - don't hit eachother
    - remove when hit blocks

--- src/main/java/com/cyno/diablo/entities/DiabloFireParticleEntity.java
+++ src/main/java/com/cyno/diablo/entities/DiabloFireParticleEntity.java
@@ -3,19 +3,25 @@ package com.cyno.diablo.entities;
 import com.cyno.diablo.Diablo;
 import com.cyno.diablo.init.DiabloEntityTypes;
 import net.minecraft.block.BlockState;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.CrashReportCategory;
+import net.minecraft.crash.ReportedException;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.passive.AmbientEntity;
+import net.minecraft.util.math.*;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
+import java.util.UUID;
+
 public class DiabloFireParticleEntity extends AmbientEntity {
-    static final double SPEED = 0.75D;
-    LivingEntity shooter;
-    int age;
+    private static final double SPEED = 1.5D;
+    private UUID shooterUUID;
+    private int age;
 
     // would be private but EntityTypes needs to access it
     public DiabloFireParticleEntity(EntityType<? extends AmbientEntity> type, World world) {
@@ -23,49 +29,57 @@ public class DiabloFireParticleEntity extends AmbientEntity {
         this.setNoGravity(true);
         this.setInvulnerable(true);
         this.age = 0;
+        this.noClip = true;  // so that it doesn't stop before it hits the wall
     }
 
     // crashes if you don't do these even though they don't effect anything
     // also you need to bind this to the entity on FMLCommonSetupEvent
     public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
-        return DiabloFireParticleEntity.registerAttributes()
-                .createMutableAttribute(Attributes.MAX_HEALTH, 90.0)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.15f)
-                .createMutableAttribute(Attributes.ATTACK_KNOCKBACK, 1.0)
-                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 7.0)
-                .createMutableAttribute(Attributes.FOLLOW_RANGE, 45.0);
+        return DiabloFireParticleEntity.registerAttributes().createMutableAttribute(Attributes.FOLLOW_RANGE, 0.0);
     }
 
     @Override
     public void livingTick() {
         super.livingTick();
-        age++;
-        if (age > 250 || this.getMotion() == Vector3d.ZERO){
-            this.remove();
+
+        if (!world.isRemote()){
+            age++;
+            if (age > 40) this.remove();
+
+            // I have to call this manually becuase I set noClip to true
+            this.doBlockCollisions();
         }
     }
 
-    // create a new one, set its shooter and send it in a direction
-    public static void createNew(World world, LivingEntity shooter, float x, float z) {
+    // create a new one, set its shooter and send it in a horizontal direction (specified with degrees
+    // around a circle centered on the shooter). yOffset is how much up from the bottom of the shooter
+    // degrees can be from 0 - 360 (outside range just loops around)
+    // could fairly easily refactor to not need a shooter
+    public static void createNew(World world, LivingEntity shooter, double degrees, double yOffset) {
         DiabloFireParticleEntity particle = new DiabloFireParticleEntity(DiabloEntityTypes.DIABLO_FIRE_PARTICLE.get(), world);
 
-        particle.setPosition(shooter.getPosX(), shooter.getPosY(), shooter.getPosZ());
+        particle.setPosition(shooter.getPosX(), shooter.getPosY() + yOffset, shooter.getPosZ());
+        particle.shooterUUID = shooter.getUniqueID();
 
-        // convert degress to xy on unit circle y->z
-        Vector3d motion = (new Vector3d(x, 0, z)).normalize().scale(SPEED);
+        // convert degrees around unit circle to their point on the circle
+        double rad = Math.toRadians(degrees);
+        double x = Math.cos(rad);
+        double z = Math.sin(rad);
+
+        Vector3d motion = (new Vector3d(x, 0, z)).scale(SPEED);
         particle.setMotion(motion);
-        particle.shooter = shooter;
 
         world.addEntity(particle);
-
     }
 
     // when it hits an entity other than the shooter light them on fire and remove itself
     @Override
     protected void collideWithEntity(Entity entityIn) {
-        if (shooter == null) return;
+        super.collideWithEntity(entityIn);
 
-        boolean isShooter = entityIn.getUniqueID() == shooter.getUniqueID();
+        if (shooterUUID == null || entityIn instanceof DiabloFireParticleEntity) return;
+
+        boolean isShooter = entityIn.getUniqueID() == shooterUUID;
 
         if (!world.isRemote() && entityIn instanceof LivingEntity && !isShooter){
             entityIn.setFire(10);
@@ -73,11 +87,11 @@ public class DiabloFireParticleEntity extends AmbientEntity {
         }
     }
 
-    // when it runs into a block remove itself
+    // without noClip=true this seems to never fire becuase it stops moving before its in the block
     @Override
     protected void onInsideBlock(BlockState state) {
-       if (!world.isRemote() && state.isSolid()){
-           this.remove();
-       }
+        if (state.isSolid()){
+            this.remove();
+        }
     }
 }
