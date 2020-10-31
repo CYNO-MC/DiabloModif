3d6b30cddcfc44e12ccb49a530320553eba6f4cb
Luke Graham Landry <graham11000@gmail.com>
2020-10-31 11:54:50 -0400

    Refactor Circle Util
    - circle util stuff nolonger needs the world becuase <action> is written in the context of whatever called it so it already knows the world without explicetly passing it in
    
    - fixed crash when arson potion hit an entity because it cast to BlockRayTraceResult before checking that it hit a block

--- src/main/java/com/cyno/diablo/util/CircleHelper.java
+++ src/main/java/com/cyno/diablo/util/CircleHelper.java
@@ -1,36 +1,31 @@
 package com.cyno.diablo.util;
-import com.mojang.datafixers.types.Func;
-import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class CircleHelper {
-    private static void handleBlock(int x, int y, int z, World world, Consumer<PosAndWorld> action){
-        action.accept(new PosAndWorld(new BlockPos(x, y, z), world));
+    private static void handleBlock(int x, int y, int z, Consumer<BlockPos> action){
+        action.accept(new BlockPos(x, y, z));
     }
 
-    private static void drawCirclePart(int xc, int yc, int x, int y,  int height, World world, Consumer<PosAndWorld> action)
+    private static void drawCirclePart(int xc, int yc, int x, int y,  int height, Consumer<BlockPos> action)
     {
-        handleBlock(xc+x, height, yc+y, world, action);
-        handleBlock(xc-x, height, yc+y, world,  action);
-        handleBlock(xc+x, height, yc-y, world,  action);
-        handleBlock(xc-x, height, yc-y, world,  action);
-        handleBlock(xc+y, height, yc+x, world,  action);
-        handleBlock(xc-y, height, yc+x, world,  action);
-        handleBlock(xc+y, height, yc-x, world,  action);
-        handleBlock(xc-y, height, yc-x, world,  action);
+        handleBlock(xc+x, height, yc+y, action);
+        handleBlock(xc-x, height, yc+y,  action);
+        handleBlock(xc+x, height, yc-y,  action);
+        handleBlock(xc-x, height, yc-y,  action);
+        handleBlock(xc+y, height, yc+x,  action);
+        handleBlock(xc-y, height, yc+x,  action);
+        handleBlock(xc+y, height, yc-x,  action);
+        handleBlock(xc-y, height, yc-x,  action);
     }
 
-    // Function for circle-generation
-    // using Bresenham's algorithm
-    private static void circleBres(int xc, int yc, int r, int height, World world, Consumer<PosAndWorld> action)
+    // Function for circle-generation using Bresenham's algorithm
+    private static void circleBres(int xc, int yc, int r, int height, Consumer<BlockPos> action)
     {
         int x = 0, y = r;
         int d = 3 - 2 * r;
-        drawCirclePart(xc, yc, x, y, height, world, action);
+        drawCirclePart(xc, yc, x, y, height, action);
         while (y >= x)
         {
             // for each pixel we will
@@ -48,27 +43,14 @@ public class CircleHelper {
             }
             else
                 d = d + 4 * x + 6;
-            drawCirclePart(xc, yc, x, y, height, world, action);
+            drawCirclePart(xc, yc, x, y, height, action);
         }
     }
 
 
     // finds the position for a circle of blocks (centered on <center> with radius <radius>)
-    // and calls <action> for each of them.
-    public static void horizontalCircle(World world, BlockPos center, int radius, Consumer<PosAndWorld> action){
-        circleBres(center.getX(), center.getZ(), radius, center.getY(), world, action);
-    }
-
-    // this just exists to pass the position and the world to the consumer in one argument
-    // yes, that's dumb, I should have made my own interface or maybe java has one
-    // but I'm really tired, we can fix it later
-    public static class PosAndWorld {
-        public BlockPos pos;
-        public World world;
-
-        public PosAndWorld(BlockPos posIn, World world){
-            this.pos = posIn;
-            this.world = world;
-        }
+    // and calls <action> (method reference / lambda that takes in a BlockPos) for each of them.
+    public static void horizontalCircle(BlockPos center, int radius, Consumer<BlockPos> action){
+        circleBres(center.getX(), center.getZ(), radius, center.getY(), action);
     }
 }
