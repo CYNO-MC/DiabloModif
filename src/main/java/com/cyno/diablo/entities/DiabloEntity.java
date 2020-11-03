2dc72e463b7f871a290cf07fba0b098db07b21d2
CYNO-MC <73330031+CYNO-MC@users.noreply.github.com>
2020-11-03 10:29:49 -0500

    Changing Animation Loop

--- src/main/java/com/cyno/diablo/entities/DiabloEntity.java
+++ src/main/java/com/cyno/diablo/entities/DiabloEntity.java
@@ -174,9 +174,10 @@ public class DiabloEntity extends MonsterEntity implements IAnimatable {
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event){
 
         if (isCurrentlyGrabbing){
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.diablomodif.diablo_entity.grabattack", true));
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.diablomodif.diablo_entity.grabattack", false));
             return PlayState.CONTINUE;
         }
+
         // idle if not moving fast
         if(this.getMotion().length() < 0.06){
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.diablomodif.diablo_entity.idle", true));
