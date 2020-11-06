dc2799484894966d9f64b016a56c3832f4ff0f91
CYNO-MC <73330031+CYNO-MC@users.noreply.github.com>
2020-11-05 23:19:20 -0500

    Creating SmashAttackGoal

---
+++ src/main/java/com/cyno/diablo/goals/SmashAttackGoal.java
@@ -0,0 +1,18 @@
+package com.cyno.diablo.goals;
+
+import net.minecraft.entity.ai.goal.Goal;
+
+public class SmashAttackGoal extends Goal {
+    @Override
+    public boolean shouldExecute() {
+        return false;
+    }
+
+
+
+
+
+
+
+
+}
