6ef5a8cd6c800c77b656037025270e9f07053d53
CYNO-MC <73330031+CYNO-MC@users.noreply.github.com>
2020-11-08 17:33:30 -0500

    Fixing Ancient Blocks

--- src/main/java/com/cyno/diablo/init/DiabloBlocks.java
+++ src/main/java/com/cyno/diablo/init/DiabloBlocks.java
@@ -29,6 +29,30 @@ public class DiabloBlocks {
             .harvestLevel(3)
             .harvestTool(ToolType.PICKAXE)));
 
+    public static final RegistryObject<Block> CHISELED_ANCIENT_BRICK = BLOCKS.register("chiseled_ancient_brick", () -> new Block(Block.Properties.create(Material.ROCK)
+            .hardnessAndResistance(30f, 1200.0f)
+            .sound(SoundType.ANCIENT_DEBRIS)
+            .harvestLevel(3)
+            .harvestTool(ToolType.PICKAXE)));
+
+    public static final RegistryObject<Block> ANCIENT_FENCE = BLOCKS.register("ancient_fence", () -> new Block(Block.Properties.create(Material.ROCK)
+            .hardnessAndResistance(30f, 1200.0f)
+            .sound(SoundType.ANCIENT_DEBRIS)
+            .harvestLevel(3)
+            .harvestTool(ToolType.PICKAXE)));
+
+    public static final RegistryObject<Block> ANCIENT_WALL = BLOCKS.register("ancient_wall", () -> new Block(Block.Properties.create(Material.ROCK)
+            .hardnessAndResistance(30f, 1200.0f)
+            .sound(SoundType.ANCIENT_DEBRIS)
+            .harvestLevel(3)
+            .harvestTool(ToolType.PICKAXE)));
+
+    public static final RegistryObject<Block> ANCIENT_PLATE = BLOCKS.register("ancient_plate", () -> new Block(Block.Properties.create(Material.ROCK)
+            .hardnessAndResistance(30f, 1200.0f)
+            .sound(SoundType.ANCIENT_DEBRIS)
+            .harvestLevel(3)
+            .harvestTool(ToolType.PICKAXE)));
+
     // Nether Brick Variations
     public static final RegistryObject<Block> REINFORCED_NETHER_BRICK_BLOCK = BLOCKS.register("reinforced_nether_brick_block", () -> new Block(Block.Properties.create(Material.IRON)
             .hardnessAndResistance(50.0f, 1200f)
